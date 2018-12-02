5494edf230856c04452be0f5d77813d3e8175306
Gipsy Redman <gipsy.redman@gmail.com>
2018-12-02 18:24:19 -0500

    5.1 controller and routes edits

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages/index'
+  get 'simple_pages/landing_page'
+  get 'products/index'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/show'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
