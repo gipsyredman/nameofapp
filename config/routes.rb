640cfeeeb32983bd659becf4aad72fcd421a2247
Gipsy Redman <gipsy.redman@gmail.com>
2018-12-04 13:56:05 -0500

    5.2 fixed routes and searchbar, added some styles

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  get 'products/index'
-  get 'products/new'
-  get 'products/edit'
-  get 'products/show'
   root 'simple_pages#landing_page'
+
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
