7faacff48833525355001b86ce569cac0525aba5
Gipsy Redman <gipsy.redman@gmail.com>
2018-12-04 19:33:40 -0500

    5.3 added sendgrid plguin

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Enable ActionMailer to use SendGrid plugin to send emails from Heroku
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['app113708785@heroku.com'],
+  password: ENV['H@t3r@d3'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
