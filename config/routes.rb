840c7642481afbba363ccb57292f56f2b1f89257
Gipsy Redman <gipsy.redman@gmail.com>
2018-12-09 14:32:33 -0500

    fixed sign out error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
     get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 #  resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
