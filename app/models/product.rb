74904d01dfac963371d8bcd3ff1751870752b3ae
Gipsy Redman <gipsy.redman@gmail.com>
2018-12-11 14:23:31 -0500

    5.7 added comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ApplicationRecord
   has_many :orders
-
+  has_many :comments
   def self.search(search_term)
     if Rails.env.production?
       Product.where("name ILIKE ?", "%#{search_term}%")
     else
       Product.where("name LIKE ?", "%#{search_term}%")
     end
+  end
+  def highest_rating_comment
+    comments.rating_desc.first
   end
 end
