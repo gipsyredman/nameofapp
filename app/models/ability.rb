2b423d2492a7fb5e5af9c10132e75c0af033618a
Gipsy Redman <gipsy.redman@gmail.com>
2018-12-27 19:57:44 -0500

    5.10 added admin flag and restricted access

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :manage, Comment,where(user_id: user.id) do |comment|
+        comment.user_id == user.id
+      end
+      cannot [:destroy, :delete], Comment.where(user_id: user.id) do |comment|
+      end
+    end
   end
 end
