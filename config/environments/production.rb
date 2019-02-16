e7cf70cf5e206b21001c752b5ae79f6111b04075
Gipsy Redman <gipsy.redman@gmail.com>
2019-02-16 16:48:06 -0500

    6.13 ActionCable added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://queerbroads.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://queerbroads.herokuapp.com', 'http://queerbroads.herokuapp.com']
 end
