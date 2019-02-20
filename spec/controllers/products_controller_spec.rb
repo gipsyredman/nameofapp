d53471bd8f2b2e7f361f23b27dfa2866ef093633
Gipsy Redman <gipsy.redman@gmail.com>
2019-02-19 19:22:49 -0500

    added tests for controllers

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe ProductsController, type: :controller do
   before do
     @user = FactoryBot.create(:user)
-    let(:product){ Product.create!(name: "Sunglasses")}
+    @product = FactoryBot.create(:product)
   end
 
   describe 'GET #index' do
@@ -13,11 +13,52 @@ describe ProductsController, type: :controller do
       expect(response).to render_template('index')
     end
   end
-
     context 'GET #show' do
       it 'renders login page' do
         get :show, params: {id: @product}
-        expect(response).to redirect_to new_user_session_path
+        expect(response).to be_ok
       end
     end
-end
+
+    context 'GET #new' do
+      before do
+        sign_in @user
+      end
+      it 'redirects to new product page' do
+        get :new, params: {id: @product}
+        expect(response).to be_ok
+    end
+  end
+
+    context 'GET #edit' do
+      before do
+      sign_in @user
+    end
+    it 'redirects to edit product page' do
+      get :edit, params: {id: @product}
+      expect(response).to be_ok
+    end
+  end
+    describe 'POST #create' do
+    before do
+      sign_in @user
+    end
+    it 'creates a new product' do
+      expect(response).to be_successful
+    end
+    it 'cannot create new product' do
+      expect(Product.new).not_to be_valid
+    end
+  end
+
+    context 'DELETE' do
+      before do
+        sign_in @user
+      end
+
+      it 'can delete a product' do
+        delete :destroy, params: { id: @product.id }
+        expect(response).to redirect_to products_url
+      end
+    end
+  end
