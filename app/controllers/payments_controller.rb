d148f6f6dd9ae08425a6963c7a3925ba2e962c9f
Gipsy Redman <gipsy.redman@gmail.com>
2019-02-27 12:28:27 -0500

    added conditional to payment for login

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,30 +2,31 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    token = params[:stripeToken]
-    @product = Product.find(params[:product_id])
-    @user = current_user
+  @product = Product.find(params[:product_id])
+  @user = current_user
+  token = params[:stripeToken]
+  # Create the charge on Stripe's servers - this will charge the user's card
+  begin
+    charge = Stripe::Charge.create(
+       :amount => (@product.price * 100).to_i,
+       :currency => "eur",
+       :source => token,
+       :description => params[:stripeEmail],
+       :receipt_email => params[:stripeEmail]
+     )
 
-    # This will charge the user's card:
-    begin
-      charge = Stripe::Charge.create(
-        :amount => (@product.price * 100).to_i,
-        :currency => "usd",
-        :source => token,
-        :description => params[:stripeEmail],
-        :receipt_email => params[:stripeEmail]
-      )
-      if charge.paid
-        Order.create(product_id: @product.id, user_id: @user.id,
-      total: @product.price.to_i)
-      UserMailer.order_created(@user,@product).deliver_now
-      end
+  if charge.paid
+    Order.create(product_id: @product.id,user_id: @user.id, total: @product.price)
+    flash[:success] = "Your payment was processed successfully"
+  end
 
-    rescue Stripe::CardError => e
-      # When the card has been declined
-      body = e.json_body
-      err = body[:error]
-      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+  rescue Stripe::CardError => e
+    # The card has been declined
+    body = e.json_body
+    err = body[:error]
+    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-   end
+  redirect_to product_path(@product), notice: "Thank you for your purchase."
+  end
+
 end
